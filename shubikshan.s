a6a719fed36cc312a408717c22bea2b3d728f054
shubikshan <shubikshan656@gmail.com>
2025-03-06 21:14:38 -0800

    Create shubikshan.s

---
+++ shubikshan.s
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+   int i;
+   printf("Enter the number");
+   scanf("%d",&i);
+   if (n%3==0&&n%==0)
+   {
+      printf("Fizz Buzz");
+   }
+   else if  (n%3==0)
+   {
+      printf("Fizz");
+   }
+   else if(n%5==0)
+   {
+     printf("Buzz");
+   } 
+   return 0;
+}
